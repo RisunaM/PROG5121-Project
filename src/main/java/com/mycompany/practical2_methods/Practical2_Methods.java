4f090057a302559c0ec1832b65939b03c77504c5
risun <risun@CW-TD-CM02.carewell.local>
2026-09-08 18:21:05 +0200

    My First Versioning

--- src/main/java/com/mycompany/practical2_methods/Practical2_Methods.java
+++ src/main/java/com/mycompany/practical2_methods/Practical2_Methods.java
@@ -3,14 +3,28 @@
  */
 
 package com.mycompany.practical2_methods;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author risun
  */
 public class Practical2_Methods {
-
+//We are creating a method to add two numbers
+    //We are going to make use of two data TYPES, String for char, int for num.
+   public static int calculateSum(int number1, int number2){
+   return number1 + number2;
+   }
+   //This is our main method, this method is embedded/ comes with the project. 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    //Ask user for first number using GUI
+    String Num1 =JOptionPane.showInputDialog(null,"Enter the first number");
+    String Num2 =JOptionPane.showInputDialog(null,"Enter the Second number");
+    //Convert string input to integer
+    int firstNum = Integer.parseInt(Num1);
+    int secondNum = Integer.parseInt(Num2);
+    
+    int result = calculateSum(firstNum, secondNum);
+    JOptionPane.showMessageDialog(null,"The answer is " + result);
     }
 }
+//We cannot put anything outside this class for now
